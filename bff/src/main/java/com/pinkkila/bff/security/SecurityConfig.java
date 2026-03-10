d99df5ef7ea3e8821293e7567f24adc66e0b4581
pinkkila <petteri.pinkkila@outlook.com>
2026-03-10 14:45:57 +0200

    add initial bff config

---
+++ bff/src/main/java/com/pinkkila/bff/security/SecurityConfig.java
@@ -0,0 +1,86 @@
+package com.pinkkila.bff.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.DelegatingAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.CompositeLogoutHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfLogoutHandler;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Value("${app.base-uri}")
+    private String appBaseUri;
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        CookieCsrfTokenRepository cookieCsrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName(null);
+        // @formatter:off
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .anyRequest().authenticated()
+                )
+                .csrf(csrf ->
+                        csrf
+                                .csrfTokenRepository(cookieCsrfTokenRepository)
+                                .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                )
+//                .cors(Customizer.withDefaults())
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(authenticationEntryPoint())
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .successHandler(new SimpleUrlAuthenticationSuccessHandler(this.appBaseUri)))
+                .logout(logout ->
+                        logout
+                                .addLogoutHandler(logoutHandler(cookieCsrfTokenRepository))
+                                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                )
+                .oauth2Client(Customizer.withDefaults());
+        // @formatter:on
+        return http.build();
+    }
+    
+    
+    private AuthenticationEntryPoint authenticationEntryPoint() {
+        return DelegatingAuthenticationEntryPoint.builder()
+                .addEntryPointFor(
+                        new LoginUrlAuthenticationEntryPoint("/oauth2/authorization/bff-client"),
+                        new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                )
+                .defaultEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .build();
+    }
+    
+    private LogoutHandler logoutHandler(CsrfTokenRepository csrfTokenRepository) {
+        return new CompositeLogoutHandler(
+                new SecurityContextLogoutHandler(),
+                new CsrfLogoutHandler(csrfTokenRepository));
+    }
+    
+}
+
